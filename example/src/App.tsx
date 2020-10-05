78a5b4a646c12474ae55f0a0079dd3b6cdf37f22
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-10-05 13:35:25 +0200

    added input types to example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,41 +1,53 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 
 import { Form } from 'react-form-from-json'
 import 'react-form-from-json/dist/index.css'
 
 const App = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>, payload: {}) => {
+    e.preventDefault()
+    console.log('HandleSubmit -> payload', payload)
+  }
+
   const fields = [
     {
       name: 'Dummy email',
-      type: 'email'
+      inputType: 'email',
+      type: 'text'
     },
     {
       name: 'Dummy number',
-      type: 'number'
+      inputType: 'number',
+      type: 'text'
     },
     {
       name: 'Dummy password',
-      type: 'password'
+      inputType: 'password',
+      type: 'text'
     },
     {
       name: 'Dummy search',
-      type: 'search'
+      inputType: 'search',
+      type: 'text'
     },
     {
       name: 'Dummy tel',
-      type: 'tel'
+      inputType: 'tel',
+      type: 'text'
     },
     {
       name: 'Dummy text',
+      inputType: 'text',
       type: 'text'
     },
     {
       name: 'Dummy url',
-      type: 'url'
+      tyinputTypepe: 'url',
+      type: 'text'
     }
   ]
 
-  return <Form fields={fields} />
+  return <Form fields={fields} submitAction={handleSubmit} />
 }
 
 export default App
